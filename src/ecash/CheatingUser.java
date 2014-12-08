9f47a1dfbbd872fb7240547799c8497bef429afd
Zeohye <Nilshenning@live.dk>
2014-12-08 19:39:55 +0100

    Made a user that can cheat by spending coins more than once

---
+++ src/ecash/CheatingUser.java
@@ -0,0 +1,33 @@
+import exception.*;
+import exception.DoubleDepositException;
+import exception.DoubleSpendingException;
+import exception.InvalidPidException;
+import exception.NoCoinException;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Gamer PRO on 12/8/2014.
+ */
+public class CheatingUser extends User {
+    private LinkedList<Coin> spendCoins = new LinkedList<Coin>();
+
+    public CheatingUser(int U, Bank bank) {
+        super(U, bank);
+    }
+
+    public User spendCoin(Shop shop) throws exception.InvalidCoinException, InvalidPidException, NoCoinException, DoubleDepositException, DoubleSpendingException {
+        spendCoins.add(super.coins.getFirst());
+        super.spendCoin(shop);
+        return this;
+    }
+
+    public User spendUsedCoin(Shop shop) throws exception.InvalidCoinException, InvalidPidException, NoCoinException, DoubleDepositException, DoubleSpendingException {
+        if(spendCoins.size() == 0) throw new NoCoinException();
+        int pid = shop.getpid();
+        Coin c = spendCoins.removeFirst();
+        Pair sigma = Util.OTSign(c.sk, pid);
+        shop.buy(c.vk, c.sigmaB, sigma, pid);
+        return this;
+    }
+}
